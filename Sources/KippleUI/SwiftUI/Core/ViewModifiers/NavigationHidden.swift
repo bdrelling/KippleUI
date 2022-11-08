6bc951062970301a3a3480664034334017f028ed
Brian Drelling <brian@drelling.com>
2022-11-08 13:44:07 -0600

    Fixed bug with availability check

--- Sources/KippleUI/SwiftUI/Core/ViewModifiers/NavigationHidden.swift
+++ Sources/KippleUI/SwiftUI/Core/ViewModifiers/NavigationHidden.swift
@@ -1,24 +1,23 @@
 // Copyright © 2022 Brian Drelling. All rights reserved.
 
+#if !os(macOS)
+
 import Foundation
 import SwiftUI
 
-@available(macOS, unavailable)
 public struct NavigationHidden: ViewModifier {
     public func body(content: Content) -> some View {
-        // FIXME: It's absurd that this is required, but the unavailable attribute is not being respected.
-        if #available(macOS 13.0, *) {
-            content
-                .navigationBarTitle("")
-                .navigationBarHidden(true)
-                .navigationBarBackButtonHidden(true)
-        }
+        content
+            .navigationBarTitle("")
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
     }
 }
 
-@available(macOS, unavailable)
 public extension View {
     func navigationHidden() -> some View {
         self.modifier(NavigationHidden())
     }
 }
+
+#endif
