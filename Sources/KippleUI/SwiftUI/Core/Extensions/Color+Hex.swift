cbed22b9059893e3f26d2dcccdcb850a706d4863
Brian Drelling <brian@drelling.com>
2023-09-19 15:10:45 -0500

    Updated Color+Hex to include Int and String extensions

--- Sources/KippleUI/SwiftUI/Core/Extensions/Color+Hex.swift
+++ Sources/KippleUI/SwiftUI/Core/Extensions/Color+Hex.swift
@@ -2,39 +2,27 @@
 
 import SwiftUI
 
-// source: https://blog.eidinger.info/from-hex-to-color-and-back-in-swiftui
 public extension Color {
-    init?(hex: String) {
-        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
-        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+    init(hex: UInt, alpha: Double = 1.0) {
+        let red = Double((hex >> 16) & 0xFF) / 255.0
+        let green = Double((hex >> 8) & 0xFF) / 255.0
+        let blue = Double(hex & 0xFF) / 255.0
 
-        var rgb: UInt64 = 0
+        self.init(red: red, green: green, blue: blue, opacity: alpha)
+    }
 
-        var r: CGFloat = 0.0
-        var g: CGFloat = 0.0
-        var b: CGFloat = 0.0
-        var a: CGFloat = 1.0
+    init(hex hexString: String, alpha: Double = 1.0) {
+        var formattedHexString = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
 
-        let length = hexSanitized.count
-
-        guard Scanner(string: hexSanitized).scanHexInt64(&rgb) else { return nil }
-
-        if length == 6 {
-            r = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
-            g = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
-            b = CGFloat(rgb & 0x0000FF) / 255.0
-
-        } else if length == 8 {
-            r = CGFloat((rgb & 0xFF000000) >> 24) / 255.0
-            g = CGFloat((rgb & 0x00FF0000) >> 16) / 255.0
-            b = CGFloat((rgb & 0x0000FF00) >> 8) / 255.0
-            a = CGFloat(rgb & 0x000000FF) / 255.0
-
-        } else {
-            return nil
+        if formattedHexString.count == 3 {
+            formattedHexString = String(formattedHexString.flatMap { [$0, $0] })
         }
 
-        self.init(red: r, green: g, blue: b, opacity: a)
+        if let hexValue = UInt(formattedHexString, radix: 16) {
+            self.init(hex: hexValue, alpha: alpha)
+        } else {
+            self = .black
+        }
     }
 }
 
