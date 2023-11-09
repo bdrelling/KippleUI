a3a30c4e60080501187dac9ae9bb34f723f4968a
Brian Drelling <brian@drelling.com>
2023-11-09 00:11:27 -0600

    Added reverseMask

---
+++ Sources/KippleUI/SwiftUI/Core/ViewModifiers/View+ReverseMask.swift
@@ -0,0 +1,33 @@
+// Copyright © 2023 Brian Drelling. All rights reserved.
+
+import SwiftUI
+
+// Source: https://www.fivestars.blog/articles/reverse-masks-how-to/
+public extension View {
+    func reverseMask<Mask: View>(
+        alignment: Alignment = .center,
+        @ViewBuilder _ mask: () -> Mask
+    ) -> some View {
+        self.mask {
+            Rectangle()
+                .overlay(alignment: alignment) {
+                    mask()
+                        .blendMode(.destinationOut)
+                }
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct View_ReverseMask_Previews: PreviewProvider {
+    static var previews: some View {
+        Color.blue
+            .reverseMask {
+                Text("Reverse!")
+                    .font(.largeTitle.bold())
+            }
+            .padding()
+            .background(.yellow)
+    }
+}
