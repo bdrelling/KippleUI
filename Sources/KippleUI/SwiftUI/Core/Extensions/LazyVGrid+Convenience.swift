e6abb78437949f4d10ac95d89d632ede1cb21cce
Brian Drelling <brian@drelling.com>
2024-02-16 12:29:31 -0600

    Modified LazyVGrid

--- Sources/KippleUI/SwiftUI/Core/Extensions/LazyVGrid+Convenience.swift
+++ Sources/KippleUI/SwiftUI/Core/Extensions/LazyVGrid+Convenience.swift
@@ -6,15 +6,35 @@ import SwiftUI
 public extension LazyVGrid {
     init(
         columns: Int,
+        gridItem: GridItem = .init(.flexible()),
         alignment: HorizontalAlignment = .center,
         spacing: CGFloat? = nil,
         pinnedViews: PinnedScrollableViews = .init(),
         @ViewBuilder content: () -> Content
     ) {
         self.init(
-            columns: Array(repeating: .init(.flexible()), count: columns),
+            columns: Array(repeating: gridItem, count: columns),
             alignment: alignment,
             spacing: spacing,
+            pinnedViews: pinnedViews,
+            content: content
+        )
+    }
+    
+    init(
+        columns: Int,
+        gridItemSize: GridItem.Size = .flexible(),
+        alignment: HorizontalAlignment = .center,
+        verticalSpacing: CGFloat? = nil,
+        horizontalSpacing: CGFloat? = nil,
+        pinnedViews: PinnedScrollableViews = .init(),
+        @ViewBuilder content: () -> Content
+    ) {
+        self.init(
+            columns: columns,
+            gridItem: .init(.flexible(), spacing: horizontalSpacing),
+            alignment: alignment,
+            spacing: verticalSpacing,
             pinnedViews: pinnedViews,
             content: content
         )
