26590adb027909e19cb0e168930fc8dba0d86602
Brian Drelling <brian@drelling.com>
2024-05-16 15:22:47 -0500

    Stopped FontIterator from printing

--- Sources/KippleFonts/Views/FontIterator.swift
+++ Sources/KippleFonts/Views/FontIterator.swift
@@ -10,15 +10,6 @@ public struct FontIterator<Content>: View where Content: View {
         ForEach(self.fonts, id: \.self) { font in
             self.content(Font(font), font.safeFamilyName)
         }
-        .onAppear {
-            for font in self.fonts.sorted(by: { $0.safeFamilyName < $1.safeFamilyName }) {
-                print("---")
-                print("Font: \(font)")
-                print("Family Name: \(font.safeFamilyName)")
-                print("Descriptor: \(font.fontDescriptor)")
-                print("---")
-            }
-        }
     }
 
     public init(
@@ -36,24 +27,6 @@ struct FontIterator_Previews: PreviewProvider {
     private static let pangram = "How vexingly quick daft zebras jump!"
 
     static var previews: some View {
-//        ScrollView {
-//            LazyVStack(alignment: .leading, spacing: 16) {
-//                FontIterator { font, familyName in
-//                    VStack(alignment: .leading, spacing: 4) {
-//                        Text(familyName)
-//                            .font(.caption)
-//                            .foregroundColor(.secondary)
-//
-//                        Text(self.pangram)
-//                            .font(font)
-//                    }
-//                    .fixedSize(horizontal: false, vertical: true)
-//                }
-//            }
-//            .padding()
-//            .frame(maxWidth: .infinity)
-//        }
-
         FontTester(
             color: .whiteOnBlack,
             textStyle: .body
